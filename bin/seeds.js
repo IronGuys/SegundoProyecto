a7dc3f9f0c7b14c4bc5a8d546b5142d711becbae
Alfio <alfioanr@gmail.com>
2018-02-12 16:34:07 +0100

    base de datos y app sync

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,26 +1,36 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
-const User = require("../models/user");
-const Products = require("../models/products");
+const User = require("../models/User");
+const Product = require("../models/Product");
 
-mongoose.connect("mongodb://localhost/HipStore");
+
+mongoose
+  .connect("mongodb://localhost/HipStore") .then(() => console.log("Conectado con Seeds!"));
+;
 var salt = bcrypt.genSaltSync(bcryptSalt);
 const password = "ironguys";
 var encryptedPass = bcrypt.hashSync(password, salt);
 
-const boss = new User({
+const admin = {
   username: "admin",
   name: "HipStore",
   password: encryptedPass,
   role: "admin"
-});
-const Products = [
+};
+const productsArray =[
     {
-        name: String,
-        description: String,
-        price: Number,
-        images: url
+        name: "scissorsSet",
+        description: "the perfect sciissor set for your beard" ,
+        price: 30
 
     }
-]
+];
+Product.collection.drop();
+
+productsArray.forEach(product => {
+  let newProduct = new Product(product);
+  newProduct.save(product)
+    .then(newSavedProduct => console.log(newSavedProduct))
+    .catch(err=>console.log(err));
+});
